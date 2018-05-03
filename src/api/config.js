b7c9f5ac054be36bb3308bc4f0e29521da3964da
leradin <lenin_ramirez_diaz@hotmail.com>
2018-05-02 23:23:42 -0500

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,10 @@
+export default{
+	appname : 'platzimusic',
+	apiKey : '052226ea3f2af8a5c71b8481e1e39606',
+	secret : '54cfe0d449309ebe265b1e6aa14fb8a5',
+	registeredTo : 'leradin'
+}
+// Application name 	platzimusic
+// API key 	052226ea3f2af8a5c71b8481e1e39606
+// Shared secret 	54cfe0d449309ebe265b1e6aa14fb8a5
+// Registered to 	leradin
